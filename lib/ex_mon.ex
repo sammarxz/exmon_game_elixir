bff32d527510da384af8840d691b7155ae9329fd
Sam Marxz <sammarxzz@gmail.com>
2026-02-07 16:49:36 -0300

    craete Game agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,19 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_name "Ash"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+   create_pc_player()
+    |> Game.start(player)
+  end
+
+  defp create_pc_player do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
   end
 end
