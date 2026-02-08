428c0aeb06587cb0a8afc623dd90872e2033b04d
Sam Marxz <sammarxzz@gmail.com>
2026-02-08 12:31:12 -0300

    dealing with computer's turn

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -3,6 +3,7 @@ defmodule ExMon do
   alias ExMon.Game.{Actions, Status}
 
   @computer_name "Ash"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   def start_game(player) do
     @computer_name
@@ -25,6 +26,8 @@ defmodule ExMon do
     move
     |> Actions.fetch_move
     |> do_move()
+
+    computer_move(Game.info())
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
@@ -36,4 +39,10 @@ defmodule ExMon do
 
     Status.print_round_message(Game.info())
   end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+  defp computer_move(_), do: :ok
 end
